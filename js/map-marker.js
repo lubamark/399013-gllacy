ce16159f9ae6335301269d70021652e71d3f2d2a
lubamark <lubamark@mail.ru>
2017-12-05 23:12:47 +0700

    убрала нерабочий код из карты, цсс добавила префиксы

--- js/map-marker.js
+++ js/map-marker.js
@@ -1,30 +1,3 @@
-/* Не поняла почему не работает, 
-
-Рабочий код под комментарием, 
-перековыряла с сайта яндекса пример, сдвинула центр, убрала лишние поля и значки, копирайт убирать нельзя. 
-
-	ymaps.ready(init);
-	var myMap, 
-		myPlacemark;
-
-	function init(){
-		myMap = new ymaps.Map("yandexMap", {
-			center: [59.9387942, 30.3230833],
-			zoom: 16
-		});
-	}
-	
-	myPlacemark = new ymaps.Placemark([59.9387942, 30.3230833]),{},{
-		iconLayout: 'default#image',
-		iconImageHref: 'img/map-marker-shadow.png',
-		iconImageSize: [218, 142]
-	};
-		
-	myMap.geoObjects.add(myPlacemark);
-
-*/
-'use strict';
-
 var myPlacemark, myMap;
 
 ymaps.ready(function () {
